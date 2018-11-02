1860ff387b52f9a559c9412385732fb8d6675258
Martijn <m.reeuwijk@Live.nl>
2018-11-02 10:18:32 +0100

    First commit

---
+++ Research_plan.md
@@ -0,0 +1,42 @@
+# Research plan
+
+- Hello API (een succesvolle request waaruit je JSON haalt)
+[X] Done
+- What the zit er in de API? Krijg een idee van de informatie die je uit de API kan halen
+
+De data die je uti de API haalt is vrij uitbundig, en heeft veel verschillende Keys waar je op kan zoeken zoals bijvoorbeeld, year, publisher, language, authors. Dit is maar een stukje van de totalen verschillende keys.
+
+- Verzin onderzoeksvragen
+Hoe is de verhouding tussen verschillende genres per decenia?
+bijvoorbeeld: Verhouding tussen horror en detective boeken in de jaren 70.
+bijvoorbeeld: Verhouding tussen romance en murdermystery in de jaren 90.
+
+
+- Verzin deelvragen (zijn er sinds de tweede feministische golf meer boeken over feminisme geschreven? Worden bepaalde themas meer uitgeleend, is er een verband tussen thema en boekdikte etc. etc.)
+
+[] Zijn er door het success van Steven King meer horror boeken verchenen? om dit teemuleren.
+[] Wat is het overall meest geschreven genres.
+[] Wat is het minst geschreven genre.
+[] Wat is het dikste boek per decenia.
+[] Welke uitgever is het meest succesvolle per decenia.
+
+- Bedenk welke variablen je nodig gaat hebben om je vraag te beantwoorden.
+  - Boekdikte.
+  - Auteur.
+  - Genre.
+  - Uitgever.
+
+- Haal deze data uit de API en sla die op in jouw eigen "datastore".
+[] Done
+
+- Doorzoek die data op patronen
+- Visualiseer de patronen met D3
+- ???
+- PROFIT
+
+
+[] In de jaren 80 kwamen demeeste horror / thriller boeken uit.
+[] horror / thriller boeken zijn korter dan fantasy boeken.
+[] Steven king heeft de meeste boeken.
+[] Kinder boeken worden het meest geleend.
+[] Welke publisher heeft de meesten boeken in de bieb.
---
+++ app.js
@@ -0,0 +1,88 @@
+const OBA = require('oba-api');
+
+// Setup authentication to api server
+const client = new OBA({
+  // ProQuest API Keys
+  public: '1e19898c87464e239192c8bfe422f280'
+});
+
+// General usage:
+// client.get({ENDPOINT}, {PARAMS});
+// ENDPOINT = search | details | refine | schema | availability | holdings
+// PARAMS = API url parameter options (see api docs for more info)
+// Client returns a promise which resolves the APIs output in JSON
+// Example search to the word 'rijk' sorted by title:
+
+client.get('search', {
+    q: 'rijk',
+    sort: 'title',
+    facet: ['genre(thriller)', 'type(book)'],
+    refine: true,
+    librarian: true,
+    page: 1
+  })
+
+  .then(results => JSON.parse(results))
+  .then(results => {
+    let raw_data = raw_json(results);
+    let core_data = core_json(results);
+    let auteurs = get_auteurs(core_data);
+    let book_object = create_book_obj(core_data);
+
+    let genre_pagenum_comparing = comparing(book_object);
+
+  })
+  .catch(err => console.log(err)) // Something went wrong in the request to the API
+
+function raw_json(results) {
+  // console.log(results);
+}
+
+function core_json(results) {
+  let core_json = results.aquabrowser.results.result;
+  // console.log(core_json);
+  return core_json;
+}
+
+function get_auteurs(data) {
+  let all_authors = data.map(data => data.authors)
+  // console.log(all_authors)
+  return all_authors
+}
+
+function create_book_obj(data) {
+  let boek = []
+  data.forEach(data => {
+    // console.log(data);
+    boek.push({
+      title: (typeof data.titles['short-title'] === "undefined") ?
+        "Geen title" : data.titles['short-title']['$t'],
+
+      author: (typeof data.authors === "undefined" ||
+          typeof data.authors['main-author']['search-term'] === "undefined") ?
+        "Geen author" : data.authors['main-author']['search-term'],
+
+      publisher: (typeof data.publication === "undefined" ||
+          typeof data.publication.publishers === "undefined" ||
+          typeof data.publication.publishers.publisher['search-term'] === "undefined") ?
+        "Geen uitgever" : data.publication.publishers.publisher['search-term'],
+
+      genre: (typeof data.genres === "undefined" || typeof data.genres.genre === "undefined") ?
+        "Geen genre" : data.genres.genre['search-term'],
+
+      releasedate: (typeof data.publication === "undefined" ||
+          typeof data.publication.year['search-term'] === "undefined") ?
+        "Geen uitgever" : data.publication.year['search-term'],
+
+      pages: (typeof data.description === "undefined" ||
+          typeof data.description['physical-description'] === "undefined") ?
+        "Geen pages" : data.description['physical-description']['$t'],
+
+      id: (typeof data.id['$t'] === "undefined" ||
+          typeof data.id === "undefined") ?
+        "Ik heb geen id ¯\_(ツ)_/¯ " : data.id['$t']
+    })
+  })
+  console.log(boek);
+  return boek
+}
